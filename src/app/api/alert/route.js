ac0a0e11c61a63c1f626aead824fe14b65170f72
arthur mac <arthur.com50@gmail.com>
2025-05-06 12:35:08 -0300

    add `AUTHORIZED_IPS` list in `/api/alert` endpoint

--- src/app/api/alert/route.js
+++ src/app/api/alert/route.js
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 import { notifyClients } from "../stream/route";
 
+const AUTHORIZED_IPS = ["192.168.1.100", "192.168.1.101", "192.168.1.102"];
+
 export async function POST(req) {
   const { ip } = await req.json();
 
   if (!ip) {
     return NextResponse.json({ message: "IP inválido" }, { status: 400 });
+  }
+
+  if (!AUTHORIZED_IPS.includes(ip)) {
+    return NextResponse.json({ message: "IP não autorizado" }, { status: 403 });
   }
 
   console.log("new ip recived:", ip);
